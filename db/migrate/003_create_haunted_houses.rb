685c5b8f446cd55f208bf88fbf28a4886a555ad5
Cody Dupuis <ctdupuis7@gmail.com>
2019-11-18 04:21:40 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,8 +3,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
     create_table :haunted_houses do |h|
       h.string :name 
       h.string :location
-      h.string :theme
       h.float :price
+      h.string :theme
       h.boolean :family_friendly
       h.datetime :opening_date
       h.datetime :closing_date
